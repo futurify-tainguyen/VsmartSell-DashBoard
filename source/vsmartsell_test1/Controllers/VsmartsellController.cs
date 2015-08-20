33452ef665f588296970ae6b9bf360501dfc1e5d
futurify-tainguyen <tainguyenx3@gmail.com>
2015-08-20 15:36:06 +0700

    add ga-date-range.js, add getGD(), add sort, add comment in vsmartsellcontroller

--- source/vsmartsell_test1/Controllers/VsmartsellController.cs
+++ source/vsmartsell_test1/Controllers/VsmartsellController.cs
@@ -13,6 +13,8 @@ namespace vsmartsell_test1.Controllers
     public class vsmartsellController : Controller
     {
         private VsmartsellDBContext db = new VsmartsellDBContext();
+
+        // lay danh sach lich su giao dich cua 1 khach hang
         public ActionResult GetListGD(int? id)
         {
             var ListGD = from m in db.DSLichSuGD
@@ -20,13 +22,24 @@ namespace vsmartsell_test1.Controllers
                          select m;
             return Json(new { ListGD = ListGD }, JsonRequestBehavior.AllowGet);
         }
-        public ActionResult UpdateHistory(int makh, DateTime ngayhethan, int sotien, string nguoithu)
+        // lay 1 lich su giao dich tham chieu theo MaGD
+        public ActionResult GetGD(int? id)
+        {
+            var lsgd = from m in db.DSLichSuGD
+                       where m.MaGD == id
+                       select m;
+            return Json( lsgd, JsonRequestBehavior.AllowGet);
+        }
+
+
+        // them data vao danh sach lich su giao dich
+        public ActionResult UpdateHistory(int makh, DateTime ngaygd, DateTime ngayhethan, int sotien, string nguoithu)
         {
             if (ModelState.IsValid)
             {
                 LichSuGD newls = new LichSuGD();
                 newls.MaKH = makh;
-                newls.NgayGD = DateTime.Now;
+                newls.NgayGD = ngaygd;
                 newls.NgayHetHan = ngayhethan;
                 newls.SoTien = sotien;
                 newls.NguoiThu = nguoithu;
@@ -37,6 +50,7 @@ namespace vsmartsell_test1.Controllers
             return Json(false);
         }
         // GET: /Vsmartsell/
+        // lay thong tin 1 khach hang
         public ActionResult GetKH(int id)
         {
             var khachhang = from m in db.DSKhachHang
@@ -52,25 +66,82 @@ namespace vsmartsell_test1.Controllers
                           select m;
             return Json(new { GiaTien = GiaTien }, JsonRequestBehavior.AllowGet);
         }
+
+        // lay danh sach gia tien cua cac loai goi
         public ActionResult GetListGiaTien()
         {
             var ListGiaTien = from m in db.DSGia
                           select m;
             return Json(new { ListGiaTien = ListGiaTien }, JsonRequestBehavior.AllowGet);
         }
+
+        // lay danh sach tat ca khach hang
         public ActionResult GetListKH()
         {
             var ListKH = from m in db.DSKhachHang
                          select m;
             return Json(new { ListKH = ListKH }, JsonRequestBehavior.AllowGet);
         }
-        public ActionResult GetList10KH(int id)
+
+        // lay danh sach 10 khach hang tuy theo so trang
+        public ActionResult GetList10KH(int id, string sorttype)
         {
             //id is page number
             var ListKH = from m in db.DSKhachHang
                          where m.Archive == false
-                         orderby m.MaKH
                          select m;
+            if (sorttype == null)
+            {
+                ListKH = ListKH.OrderBy(m => m.MaKH);
+            }
+            else if (sorttype == "kh1")
+            {
+                ListKH = ListKH.OrderBy(m => m.TenKH);
+            }
+            else if (sorttype == "kh2")
+            {
+                ListKH = ListKH.OrderByDescending(m => m.TenKH);
+            }
+            else if (sorttype == "loai1")
+            {
+                ListKH = ListKH.OrderBy(m => m.LoaiKH);
+            }
+            else if (sorttype == "loai2")
+            {
+                ListKH = ListKH.OrderByDescending(m => m.LoaiKH);
+            }
+            else if (sorttype == "goi1")
+            {
+                ListKH = ListKH.OrderBy(m => m.LoaiGoi);
+            }
+            else if (sorttype == "goi2")
+            {
+                ListKH = ListKH.OrderByDescending(m => m.LoaiGoi);
+            }
+            else if (sorttype == "batdau1")
+            {
+                ListKH = ListKH.OrderBy(m => m.NgayDangKy);
+            }
+            else if (sorttype == "batdau2")
+            {
+                ListKH = ListKH.OrderByDescending(m => m.NgayDangKy);
+            }
+            else if (sorttype == "ketthuc1")
+            {
+                ListKH = ListKH.OrderBy(m => m.NgayHetHan);
+            }
+            else if (sorttype == "ketthuc2")
+            {
+                ListKH = ListKH.OrderByDescending(m => m.NgayHetHan);
+            }
+            else if (sorttype == "ch1")
+            {
+                ListKH = ListKH.OrderBy(m => m.TenCH);
+            }
+            else if (sorttype == "ch2")
+            {
+                ListKH = ListKH.OrderByDescending(m => m.TenCH);
+            }
             var List10KH = ListKH.Skip((id-1)*10).Take(10);
             return Json(new { List10KH = List10KH }, JsonRequestBehavior.AllowGet);
         }
@@ -86,7 +157,7 @@ namespace vsmartsell_test1.Controllers
             return View();
         }
 
-        //change archive bool
+        // thay doi trang thai archive cua 1 khach hang
         [HttpPost]
         public ActionResult EditArchive(int? id)
         {
